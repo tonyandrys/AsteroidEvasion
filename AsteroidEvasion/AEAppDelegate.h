2ad85eb88679df8faf812a628d50b52398098882
Patrick <patrick-walsh@uiowa.edu>
2014-04-24 23:38:05 -0500

    Connectivity added

--- AsteroidEvasion/AEAppDelegate.h
+++ AsteroidEvasion/AEAppDelegate.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MCManager.h"
 
 @interface AEAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) MCManager *mcManager;
 
 @end
---
+++ AsteroidEvasion/MCManager.h
@@ -0,0 +1,22 @@
+//
+//  MCManager.h
+//  MCDemo
+//
+//  Created by Patrick Walsh
+//
+
+#import <Foundation/Foundation.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+
+@interface MCManager : NSObject <MCSessionDelegate>
+
+@property (nonatomic, strong) MCPeerID *peerID;
+@property (nonatomic, strong) MCSession *session;
+@property (nonatomic, strong) MCBrowserViewController *browser;
+@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
+
+-(void)setupPeerAndSessionWithDisplayName:(NSString *)displayName;
+-(void)setupMCBrowser;
+-(void)advertiseSelf:(BOOL)shouldAdvertise;
+
+@end
