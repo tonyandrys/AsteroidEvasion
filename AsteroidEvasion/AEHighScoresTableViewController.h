a3ec7b9d84b5adc7f319899c62c6f2b72be36912
Tony Andrys <tonyandrys@gmail.com>
2014-05-08 23:32:46 -0500

    Refactored high score display controllers

---
+++ AsteroidEvasion/AEHighScoresTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  AEHighScoresTableViewController.h
+//  AsteroidEvasion
+//
+//  Created by Tony Andrys on 5/8/14.
+//  Copyright (c) 2014 Tony Andrys, Ian Brauer, Patrick Walsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AEHighScoresTableViewController : UITableViewController
+
+// Segmented Control view embedded in the top navigation bar. User toggles this to swap between 1 player and 2 player scores
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *dataSourceSegmentedControl;
+
+@end
