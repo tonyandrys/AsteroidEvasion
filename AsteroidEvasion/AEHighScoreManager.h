f02b8e4d831eaaba9eace48b01856b7b200c49c7
Tony Andrys <tonyandrys@gmail.com>
2014-05-10 23:15:08 -0500

    More work on high score data model

--- AsteroidEvasion/AEHighScoreManager.h
+++ AsteroidEvasion/AEHighScoreManager.h
@@ -10,7 +10,16 @@
 /*
  AEHighScoreManager is the class responsible for return the current list of high scores, adding new elements to the table, and removing them.
  
- Do *not* instantiate this class-- all methods you should need are class-based methods.
+ High Score Table data representation
+ 
+ Structure:
+ { "name0" -> "score0", "name1" -> "score1", ... , "name(n-1) -> "score(n-1)" }
+ 
+ High scores are sorted in **descending order** by score. New elements are added such that the high score table remains sorted. This is important, as the view countroller should not be responsible for sorting.
+ 
+ All arrays returned from this class are immutable - changes should be made using addScoreToHighScoreTable and clearHighScoreTable
+ 
+ Do not instantiate this class. It will get you nowhere. All methods you should need are class-based methods.
  */
 
 // Use TABLE_ONE/TWO_PLAYER_SCORES constants to specify which table to target when calling methods with the 'table' argument
@@ -18,10 +27,15 @@
 
 extern NSString *const TABLE_ONE_PLAYER_SCORES;
 extern NSString *const TABLE_TWO_PLAYER_SCORES;
+extern NSString *const KEY_HIGH_SCORE_PLAYER_NAME;
+extern NSString *const KEY_HIGH_SCORE_PLAYER_SCORE;
 
-+(NSDictionary *)getHighScoreTable:(NSString *)table score:(NSInteger)newScore;
+// High score tables are built using a combination of NSArrays and NSDictionaries. The array returned contains one NSDictionary, each representing one high score.
 
-+(void)addScoreToHighScoreTable:(NSString *)table score:(NSInteger)newScore;
++(NSArray *)getHighScoreTable:(NSString *)table score:(NSInteger)newScore;
+
++(void)addScoreToHighScoreTable:(NSString *)table playerName:(NSString *)name score:(NSInteger)score;
+
 +(void)clearHighScoreTable:(NSString *)table;
 
 @end
--- AsteroidEvasion/AEHighScoreManager.m
+++ AsteroidEvasion/AEHighScoreManager.m
@@ -14,45 +14,127 @@
 NSString *const TABLE_ONE_PLAYER_SCORES = @"onePlayerHighScoreTable";
 NSString *const TABLE_TWO_PLAYER_SCORES = @"twoPlayerHighScoreTable";
 
-// Retrieves either the one or two player high scores from memory and returns them as a dictionary.
-+(NSDictionary *)getHighScoreTable:(NSString *)table {
+// Keys to index each dictionary stored in the high score array
+NSString *const KEY_HIGH_SCORE_PLAYER_NAME = @"highScorePlayerName";
+NSString *const KEY_HIGH_SCORE_PLAYER_SCORE = @"highScorePlayerScore";
+
+// Retrieves either the one or two player high scores from memory and returns them.
++(NSArray *)getHighScoreTable:(NSString *)table {
     
     NSUserDefaults *userPrefs = [NSUserDefaults standardUserDefaults];
     
     // One or Two player scores makes no difference in the retrieval process
     if ([table isEqualToString:TABLE_ONE_PLAYER_SCORES] || [table isEqualToString:TABLE_TWO_PLAYER_SCORES]) {
         
-        NSDictionary *scoreDictionary = [userPrefs valueForKey:table];
+        NSArray *scoreArray = [userPrefs valueForKey:table];
         // if a valid table key is passed to this method and nothing is returned, the tables do not exist. Create them and return an empty dictionary.
-        if (scoreDictionary == nil) {
+        if (scoreArray == nil) {
             NSLog(@"It looks like the high score table requested for table %@ doesn't exist. Creating them now.", table);
-            [userPrefs setObject:[NSDictionary dictionary] forKey:table];
+            [userPrefs setObject:[NSArray array] forKey:table];
             [userPrefs synchronize];
         } else {
             NSLog(@"Found an existing high score table (%@)! Returning it.", table);
-            return scoreDictionary;
+            return scoreArray;
         }
-        
-        return [NSDictionary dictionary];
+        return [NSArray array];
     }
     
     // Unrecognized table
     else {
-        NSLog(@"WARNING: Couldn't find the high score table you requested. Returning an empty dictionary.");
-        return [NSDictionary dictionary];
+        NSLog(@"WARNING: Couldn't find the high score table you requested. Returning an empty array.");
+        return [NSArray array];
     }
 
 }
 
 // Adds a score to either the one player or two player high score tables
-+(void)addScoreToHighScoreTable:(NSString *)table score:(NSInteger)newScore {
++(void)addScoreToHighScoreTable:(NSString *)table playerName:(NSString *)name score:(NSInteger)score {
     
+    // Get the requested high score table and copy it into a mutable array to make changes
+    NSArray *a = [self getHighScoreTable:table];
+    NSMutableArray *scoreArray = [NSMutableArray arrayWithArray:a];
+    
+    // Create a new dictionary to store the score we are about to add to the table
+    NSMutableDictionary *newHighScoreEntry = [NSMutableDictionary dictionary];
+    [newHighScoreEntry setValue:name forKey:KEY_HIGH_SCORE_PLAYER_NAME];
+    [newHighScoreEntry setValue:[NSString stringWithFormat:@"%i", score] forKey:KEY_HIGH_SCORE_PLAYER_SCORE];
+    NSLog(@"Created new high score entry {name: %@ -> score: %@}", name, [NSString stringWithFormat:@"%i", score]);
+    
+    // We want to add this score to the high score table while ensuring the table is still sorted. An ordered insertion is necessary.
+    
+    // Edge case I: array is empty
+    if ([scoreArray count] == 0) {
+        // A 0 size array is sorted, so just add the new score.
+        [scoreArray addObject:newHighScoreEntry];
+        NSLog(@"Added new score to the high score table (%@).", table);
+        [self writeHighScoreTable:table arrayToWrite:scoreArray];
+    
+    } else {
+        
+        // Store the index to add the new score
+        int k = 0;
+        
+        NSLog(@"Starting ordered insertion...");
+        // Perform an ordered insertion of this new score on the master high score array.
+        for (int i=0; i<[scoreArray count]; i++) {
+            NSDictionary *d = [scoreArray objectAtIndex:i];
+            NSInteger s = [[d objectForKey:KEY_HIGH_SCORE_PLAYER_SCORE] integerValue];
+            
+            // if score to add >= s, we need to add the object at index k.
+            if (score >= s) {
+                NSLog(@"Need to add score at index %i", k);
+                k = i;
+                break;
+            }
+            // If we are at the last element of the array and have not placed the score yet, the new high score is lower than all other scores in the array. We can add it to the end of the array and write the result.
+            else if (i == [scoreArray count] - 1) {
+                NSLog(@"Score is lower than every other high score so far. Adding it to the back of the array.");
+                [scoreArray addObject:newHighScoreEntry];
+                [self writeHighScoreTable:table arrayToWrite:scoreArray];
+                return;
+            }
+
+        }
+        
+        // Add the new high score at index=k by splitting the array into two parts, [0,k-1] and [k,n], and adding the new high score value to the first part.
+        
+        // Array [0,k-1]
+        NSMutableArray *a1 = [NSMutableArray array];
+        NSMutableArray *a2 = [NSMutableArray array];
+
+        for (int i=0; i<k; i++) {
+            [a1 addObject:[scoreArray objectAtIndex:i]];
+        }
+        
+        // Build array [k,n]
+        for (int i=k; i<[scoreArray count]; i++){
+            [a2 addObject:[scoreArray objectAtIndex:i]];
+        }
+             
+        // Add the new high score entry to the back of the array containing items [0,k-1]
+        [a1 addObject:newHighScoreEntry];
+        
+        // Merge the two arrays and write the result to NSUserDefaults
+        NSMutableArray *writeArray = [[NSMutableArray alloc] init];
+        [writeArray addObjectsFromArray:a1];
+        [writeArray addObject:a2];
+        [self writeHighScoreTable:table arrayToWrite:writeArray];
+    }
 }
 
 // Wipes either the 1p or 2p high score tables clean
 // (Use TABLE_ONE/TWO_PLAYER_SCORES constants to specify which table to target)
 +(void)clearHighScoreTable:(NSString *)table {
     
+}
+
+// Writes the passed array to the appropriate key of NSUserDefaults and synchronizes the changes
+// Don't call this directly unless you have a good reason-- it was written to only be called by other AEHighScoreManager methods. If you do, and arr isn't in the appropriate format (specified in the header), things will break horribly.
++(void)writeHighScoreTable:(NSString *)table arrayToWrite:(NSMutableArray *)arr {
+    NSUserDefaults *userPrefs = [NSUserDefaults standardUserDefaults];
+    [userPrefs setValue:arr forKey:table];
+    [userPrefs synchronize];
+    NSLog(@"Wrote new high score array to NSUserDefaults.");
 }
 
 @end
